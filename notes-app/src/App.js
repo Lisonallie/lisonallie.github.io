d595ae6c9fe679cd6b5cd5d246f5a43c21bc1997
Lisonallie <allisonvanlinden@gmail.com>
2019-08-21 10:07:52 +0200

    try to connect axios

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -3,13 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./components/Notes.jsx";
 import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/'
+import axios from 'axios';
 
 
 function App() {
+  componentDidMount() {
+    axios.get('http://localhost:4567/becode-database-api/list_note.php')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+   
+
   return (
-    <h1 className = "display-1">
-      Hello World!
-    </h1>
+    <div>
+      <p>
+
+      </p>
+    </div>
   );
 }
 
