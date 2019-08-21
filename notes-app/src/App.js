9e886f0d0444fe6a3af8d67d29d3c6a4654c7bf5
Lisonallie <allisonvanlinden@gmail.com>
2019-08-21 12:12:40 +0200

    add working listing code

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import "./App.css";
-import "./components/Notes.jsx";
-import "bootstrap/dist/css/bootstrap.css";
-import axios from "axios";
+import List from './components/Notes';
 
 function App() {
-  axios({
-    method: "get",
-    url: "http://localhost:4567/becode-database-api/list_note.php"
-  }).then(function(response) {
-    // location in the api response where the translation lives.
-    console.log(response.data);
-  });
 
   return (
     <div>
-      <p />
+      <List />
     </div>
   );
 }
--- notes-app/src/components/Notes.jsx
+++ notes-app/src/components/Notes.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
-function List() {
-        return ( 
-            <div>
-                <h1>Hello World!</h1>
-            </div>
-         );
-    }
- 
+class List extends Component {
+  state = {
+    notes: []
+  };
+  componentDidMount() {
+    axios({
+      method: "get",
+      url: "http://localhost:4567/becode-database-api/list_note.php"
+    }).then(response => {
+      this.setState({ notes: response.data });
+    });
+  }
+
+  render() {
+    return <p> {this.state.notes} </p>;
+  }
+}
+
 export default List;
