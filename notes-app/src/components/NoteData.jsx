dc4beb70900e8227c2799552643121e3376e3002
Lisonallie <allisonvanlinden@gmail.com>
2019-08-22 10:02:03 +0200

    rename variables logically

--- notes-app/src/components/NoteData.jsx
+++ notes-app/src/components/NoteData.jsx
@@ -4,16 +4,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const NoteData = () => {
   // note = SHOW note & setNote = GET note/give value to first note declaration
-  const [note, setNote] = useState([]);
+  const [title, setTitle] = useState([]);
   useEffect(() => {
     axios({
       method: "get",
       url: "http://localhost:4567/becode-database-api/list_note.php"
     }).then(response => {
-      setNote(response.data[0]);
+      setTitle(response.data[0]);
     });
   });
-  const listTitles = note.map((note) => <li className ="noteTitle" key={note.title}>{note.title}</li>);
+  const listTitles = title.map((title) => <li className ="noteTitle" key={title.title}>{title.title}</li>);
   
   return (
     <div className ="noteContainer" >
