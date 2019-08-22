aff868caff96a666bcd169e54dc141aff4d377ae
Lisonallie <allisonvanlinden@gmail.com>
2019-08-22 14:46:00 +0200

    remove author, fix repeating problem

--- notes-app/src/components/NoteData.jsx
+++ notes-app/src/components/NoteData.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const NoteData = () => {
-  // note = SHOW note & setNote = GET note/give value to first note declaration
   const [title, setTitle] = useState([]);
+  // title = SHOW title & setTitle = GET title/give value to first title declaration
   useEffect(() => {
     axios({
       method: "get",
@@ -12,12 +12,17 @@ const NoteData = () => {
     }).then(response => {
       setTitle(response.data[0]);
     });
-  });
-  const listTitles = title.map((title) => <li className ="noteTitle" key={title.title}>{title.title}</li>);
-  
+  },[])
+  //^^^^^add the array there to stop the response.data from repeating WAY TOO MANY TIMES
+  const listTitles = title.map(title => (
+    <li className="noteTitle" key={title.title}>
+      {title.title}
+    </li>
+  ));
+
   return (
-    <div className ="noteContainer" >
-      <ul className ="noteList" >{listTitles}</ul>
+    <div className="noteContainer">
+      <ul className="titleList">{listTitles}</ul>
     </div>
   );
 };
