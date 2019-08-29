338222fce4a3fd9c828bcccfee7f98444ada734d
Lisonallie <allisonvanlinden@gmail.com>
2019-08-29 16:11:45 +0200

    Updates

---
+++ precache-manifest.ab652f02c8a1df1117d35683d8fe63b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e154321971b9d5b0056d447f5e58f0",
+    "url": "/Lisonallie/note-app-front-end/index.html"
+  },
+  {
+    "revision": "a9d3922fa5dc478495c9",
+    "url": "/Lisonallie/note-app-front-end/static/css/2.785b6d18.chunk.css"
+  },
+  {
+    "revision": "7c600a95e1f4cb98e22b",
+    "url": "/Lisonallie/note-app-front-end/static/css/main.cca61adf.chunk.css"
+  },
+  {
+    "revision": "a9d3922fa5dc478495c9",
+    "url": "/Lisonallie/note-app-front-end/static/js/2.8d0a2980.chunk.js"
+  },
+  {
+    "revision": "7c600a95e1f4cb98e22b",
+    "url": "/Lisonallie/note-app-front-end/static/js/main.d0b3aa76.chunk.js"
+  },
+  {
+    "revision": "71913ce8a4a500a04ce6",
+    "url": "/Lisonallie/note-app-front-end/static/js/runtime~main.327c1a9c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Lisonallie/note-app-front-end/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Lisonallie/note-app-front-end/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Lisonallie/note-app-front-end/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Lisonallie/note-app-front-end/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Lisonallie/note-app-front-end/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
